0b730698ccc1f81091b791f95016adc3ac5aede4
Andrew Taylor <andrew.taylor@>
2013-06-04 11:44:25 +0100

    Make server_array ID optional

--- bin/rs-get-server-array.sh
+++ bin/rs-get-server-array.sh
@@ -2,14 +2,14 @@
 
 # rs-get-server-array <server_array_id>
 
-[[ ! $1 ]] && echo 'No server array ID provided, exiting.' && exit 1
-
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
 
-array_id="$1"
+if [ "$1" ]; then
+     array_id="/$1"
+fi
 
-url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays/$array_id"
+url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays$array_id"
 echo "GET: $url"
 api_result=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
 echo "$api_result"
